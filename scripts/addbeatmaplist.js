19d9ce3e450e533d618e74654edeb2ed09d8dd9d
bilibili <fenggaobuke@gmail.com>
2020-02-04 16:05:59 +0800

    clean up code & fix sort bug on chrome

--- scripts/addbeatmaplist.js
+++ scripts/addbeatmaplist.js
@@ -1,3 +1,13 @@
+function starname(star) {
+    if (!star) return "";
+    if (star<2) return "easy";
+    if (star<2.7) return "normal";
+    if (star<4) return "hard";
+    if (star<5.3) return "insane";
+    if (star<6.5) return "expert";
+    return "expert-plus";
+}
+
 // star: number; numerical representation of star rating
 // returns an html element used in difficulty selection menu
 function createStarRow(star) {
@@ -53,14 +63,6 @@ function createDifficultyList(boxclicked, event) {
     window.addEventListener("click", closeDifficultyList, false);
     difficultyBox.clicklistener = closeDifficultyList;
     // fill list
-    function starname(star) {
-        if (star<2) return "easy";
-        if (star<2.7) return "normal";
-        if (star<4) return "hard";
-        if (star<5.3) return "insane";
-        if (star<6.5) return "expert";
-        return "expert-plus";
-    }
     for (let i=0; i<boxclicked.data.length; ++i) {
         // add a row
         let difficultyItem = document.createElement("div");
@@ -131,96 +133,105 @@ function addBeatmapList(listurl) {
         pBeatmapCover.alt = "cover" + map.sid;
         pBeatmapCover.src = "https://cdn.sayobot.cn:25225/beatmaps/" + map.sid + "/covers/cover.webp";
         document.getElementById("beatmap-list").appendChild(pBeatmapBox);
+        pBeatmapBox.setdata = map;
         return pBeatmapBox;
     }
 
-    function starname(star) {
-        if (star<2) return "easy";
-        if (star<2.7) return "normal";
-        if (star<4) return "hard";
-        if (star<5.3) return "insane";
-        if (star<6.5) return "expert";
-        return "expert-plus";
-    }
-
-    // async
-    function addMoreInfo(box, sid) {
+    function addStarRings(box, data) {
+        // get star ratings
+        let stars = [];
+        for (let i=0; i<data.length; ++i) {
+            stars.push(data[i].star);
+        }
         let row = document.createElement("div");
         row.className = "beatmap-difficulties";
         box.appendChild(row);
-        let url = "https://api.sayobot.cn/beatmapinfo?1=" + sid;
+        // show all of them if can be fit in
+        if (stars.length <= 15) {
+            for (let i=0; i<stars.length; ++i) {
+                let difficultyRing = document.createElement("div");
+                difficultyRing.className = "difficulty-ring";
+                difficultyRing.classList.add(starname(stars[i]));
+                row.appendChild(difficultyRing);
+            }
+        }
+        // show only highest star and count otherwise
+        else {
+            let difficultyRing = document.createElement("div");
+            difficultyRing.className = "difficulty-ring";
+            difficultyRing.classList.add(starname(stars[stars.length-1]));
+            row.appendChild(difficultyRing);
+            let cnt = document.createElement("span");
+            cnt.className = "difficulty-count";
+            cnt.innerText = stars.length;
+            row.appendChild(cnt);
+        }
+        if (data.length == 0) {
+            let cnt = document.createElement("span");
+            cnt.className = "difficulty-count";
+            cnt.innerText = "no std map";
+            row.appendChild(cnt);
+        }
+    }
+    function addLength(box, data) {
+        // show length & bpm
+        let length = 0;
+        let bpm = 0;
+        for (let i=0; i<data.length; ++i) {
+            length = Math.max(length, data[i].length);
+            bpm = Math.max(bpm, data[i].BPM);
+        }
+        // let pBeatmapBPM = document.createElement("div");
+        // pBeatmapBPM.className = "beatmapbpm";
+        // box.appendChild(pBeatmapBPM);
+        // pBeatmapBPM.innerText = Math.round(bpm) + "♪";
+        let pBeatmapLength = document.createElement("div");
+        pBeatmapLength.className = "beatmaplength";
+        box.appendChild(pBeatmapLength);
+        pBeatmapLength.innerText = Math.floor(length/60) + ":" + (length%60<10?"0":"") + (length%60);
+    }
+    function addMoreInfo(box, data) {
+        // remove all but osu std mode
+        data = data.filter(function(o){return o.mode == 0;});
+        data = data.sort(function(a,b){return Math.sign(a.star-b.star);});
+        box.data = data;
+        addStarRings(box, data);
+        addLength(box, data);
+    }
+
+    // async
+    function requestMoreInfo(box) {
+        let url = "https://api.sayobot.cn/beatmapinfo?1=" + box.sid;
         let xhr = new XMLHttpRequest();
         xhr.responseType = 'text';
         xhr.open("GET", url);
         xhr.onload = function() {
             let res = JSON.parse(xhr.response);
-            // remove all but osu std mode
-            res.data = res.data.filter(function(o){return o.mode == 0;});
-            res.data = res.data.sort(function(a,b){return a.star>b.star;});
-            box.data = res.data;
-            // get star ratings
-            let stars = [];
-            for (let i=0; i<res.data.length; ++i) {
-                stars.push(res.data[i].star);
-            }
-            // show all of them if can be fit in
-            if (stars.length <= 15) {
-                for (let i=0; i<stars.length; ++i) {
-                    let difficultyRing = document.createElement("div");
-                    difficultyRing.className = "difficulty-ring";
-                    difficultyRing.classList.add(starname(stars[i]));
-                    row.appendChild(difficultyRing);
-                }
-            }
-            // show only highest star and count otherwise
-            else {
-                let difficultyRing = document.createElement("div");
-                difficultyRing.className = "difficulty-ring";
-                difficultyRing.classList.add(starname(stars[stars.length-1]));
-                row.appendChild(difficultyRing);
-                let cnt = document.createElement("span");
-                cnt.className = "difficulty-count";
-                cnt.innerText = stars.length;
-                row.appendChild(cnt);
-            }
-            if (res.data.length == 0) {
-                let cnt = document.createElement("span");
-                cnt.className = "difficulty-count";
-                cnt.innerText = "no std map";
-                row.appendChild(cnt);
-            }
-            // show length & bpm
-            let length = 0;
-            let bpm = 0;
-            for (let i=0; i<res.data.length; ++i) {
-                length = Math.max(length, res.data[i].length);
-                bpm = Math.max(bpm, res.data[i].BPM);
-            }
-            // let pBeatmapBPM = document.createElement("div");
-            // pBeatmapBPM.className = "beatmapbpm";
-            // box.appendChild(pBeatmapBPM);
-            // pBeatmapBPM.innerText = Math.round(bpm) + "♪";
-            let pBeatmapLength = document.createElement("div");
-            pBeatmapLength.className = "beatmaplength";
-            box.appendChild(pBeatmapLength);
-            pBeatmapLength.innerText = Math.floor(length/60) + ":" + (length%60<10?"0":"") + (length%60);
+            addMoreInfo(box, res.data);
         }
         xhr.send();
     }
 
+    // request beatmap pack list
     let xhr = new XMLHttpRequest();
     xhr.responseType = 'text';
     xhr.open("GET", listurl);
+    // async part 1
     xhr.onload = function() {
         let res = JSON.parse(xhr.response);
         let box = [];
+        // add widget to webpage as soon as list is fetched
         for (let i=0; i<res.data.length; ++i) {
             box.push(addpreviewbox(res.data[i]));
         }
+        // async add more info
         for (let i=0; i<res.data.length; ++i) {
-            addMoreInfo(box[i], res.data[i].sid);
+            box[i].sid = res.data[i].sid;
+            requestMoreInfo(box[i]);
             box[i].onclick = function(e) {
+                // this is effective only when box.data is available
                 createDifficultyList(box[i], e);
+                startdownload(box[i]);
             }
         }
     }
