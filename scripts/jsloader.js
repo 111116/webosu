534b34eedbcb165ee7886e704cb54496c736f984
bilibili <fenggaobuke@gmail.com>
2020-02-04 20:51:20 +0800

    implement game loader

--- scripts/jsloader.js
+++ scripts/jsloader.js
@@ -1,10 +1,15 @@
 // delayed js loader
 
-function loadScript(url, callback) {
+function loadScript(url, callback, aux) {
 	let script = document.createElement("script");
 	document.head.appendChild(script);
 	if (callback)
 		script.onload = callback;
+	if (aux) {
+		for (let key in aux) {
+			script.setAttribute(key, aux[key]);
+		}
+	}
 	script.src = url;
 }
 
@@ -12,20 +17,34 @@ function loadScript(url, callback) {
 
 window.beatmaplistLoadedCallback = function () {
 	window.setTimeout(function(){
-		loadScript("scripts/lib/zip.js", function(){window.zip.workerScriptsPath = 'scripts/lib/';});
-		loadScript("scripts/lib/zip-fs.js");
-		loadScript("scripts/lib/pixi.min.js");
-		loadScript("scripts/lib/mp3parse.min.js");
-		loadScript("scripts/lib/localforage.min.js");
-		loadScript("scripts/lib/underscore.js");
+		loadScript("scripts/lib/zip.js", function(){
+			window.zip.workerScriptsPath = 'scripts/lib/';
+			loadScript("scripts/lib/zip-fs.js", checkdep);
+		});
+		loadScript("scripts/lib/pixi.min.js", checkdep);
+		loadScript("scripts/lib/mp3parse.min.js", checkdep);
+		loadScript("scripts/lib/localforage.min.js", checkdep);
+		function checkdep() {
+			if (!window.aaaaa) window.aaaaa = 0;
+			window.aaaaa += 1;
+			if (window.aaaaa == 4) {
+				loadScript("scripts/lib/require.js", function() {
+		            require.config({
+		                paths: {
+		                    underscore: 'lib/underscore',
+		                    sound: 'lib/sound'
+		                },
+		                shim: {
+		                    "underscore": {
+		                        exports: "_"
+		                    }
+		                },
+		                urlArgs: "bust=" +  (new Date()).getTime()
+		            });
+				}, {"data-main":"scripts/initgame"});
+			}
+		}
 
-		loadScript("scripts/curves/Curve.js");
-		loadScript("scripts/curves/CurveType.js");
-		loadScript("scripts/curves/Bezier2.js");
-		loadScript("scripts/curves/CircumscribedCircle.js");
-		loadScript("scripts/curves/EqualDistanceMultiCurve.js");
-		loadScript("scripts/curves/LinearBezier.js");
-		console.log("!")
 	}, 0);
 }
 
