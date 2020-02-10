082f804257e8d5150a2016719a7ab1fa09998306
bilibili <fenggaobuke@gmail.com>
2020-02-11 00:20:46 +0800

    add version bumper

---
+++ bumpversion.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+a=$(cat .version)
+[ $# -eq 0 ] && exit 0
+b=$1
+list=$(ls *.html)
+for f in *.html
+do
+	cat "$f" | sed "s/""$a""/""$b""/" > .tmp
+	mv .tmp "$f"
+done
+echo $b > .version
