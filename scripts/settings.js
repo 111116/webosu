631d78e80c33207e7dea73e8ae1897171acfdcba
bilibili <fenggaobuke@gmail.com>
2020-02-11 00:49:01 +0800

    filter search hot key

--- scripts/settings.js
+++ scripts/settings.js
@@ -224,6 +224,8 @@ window.addEventListener('DOMContentLoaded', setOptionPanel);
 
 // press any key to search
 window.onkeydown = function(e) {
-	let textinput = document.getElementsByTagName("input")[0];
-	textinput.focus();
+	if (e.key.length == 1 && e.key != " ") {
+		let textinput = document.getElementsByTagName("input")[0];
+		textinput.focus();
+	}
 }
