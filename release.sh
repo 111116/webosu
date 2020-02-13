cd81d37a65f03e43bb69d701c84bab0e581a93af
bilibili <fenggaobuke@gmail.com>
2020-02-14 00:24:09 +0800

    release script: add js

--- release.sh
+++ release.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 dir="osuweb-""$(cat .version)"
 mkdir "$dir"
-cp *.html *.png *.svg *.json *.md LICENSE "$dir"
+cp *.js *.html *.png *.svg *.json *.md LICENSE "$dir"
 cp -r style scripts skin fonts "$dir"
 mkdir "$dir""/hitsounds"
 cp hitsounds/*.ogg "$dir""/hitsounds/"
