5f3a0710a5b3a07029498df099574d809f759fd2
bilibili <fenggaobuke@gmail.com>
2020-02-14 15:22:54 +0800

    add CORS header

--- api.js
+++ api.js
@@ -24,6 +24,7 @@ const postserver = http.createServer((req, res) => {
 const getserver = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'application/json');
+  res.setHeader('Access-Control-Allow-Origin', '*');
   res.end(JSON.stringify(a));
 });
 
